c1eae24563d40123ced7bdb4abd2eca68460df08
sirpratama <mnazirpratama@gmail.com>
2025-06-15 01:04:02 +0700

    Update Navbar links to correct 'About' section href

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 const navItems = [
     {name: "Home", href: "#hero"},
-    {name: "About", href: "#skills"},
+    {name: "About", href: "#about"},
     {name: "Skills", href: "#skills"},
     {name: "Projects", href: "#projects"},
     {name: "Contact", href: "#contact"},
